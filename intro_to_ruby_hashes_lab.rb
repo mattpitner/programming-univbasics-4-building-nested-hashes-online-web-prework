fe26ad3f6309a72369b30920ddaaa9199073c876
Matt Pitner <mattpitner09@gmail.com>
2019-08-09 21:27:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
     {
       :reading_railroad=>
       {
-       "mortgage_value"=>"$100"
+       
       },
       :pennsylvania_railroad=>
       {
@@ -74,7 +74,7 @@ def monopoly_with_fourth_tier
     {
       :reading_railroad=>
       {
-       
+       "mortgage_value"=>"$100"
       },
       :pennsylvania_railroad=>
       {
